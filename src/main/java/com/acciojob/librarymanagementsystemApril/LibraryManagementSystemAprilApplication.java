d6803f55259f016faf6c7ba418bdb645287317ca
jafar <se.hasanjafar@gmail.com>
2024-05-08 13:43:44 +0530

    Submitted12

--- src/main/java/com/acciojob/librarymanagementsystemApril/LibraryManagementSystemAprilApplication.java
+++ src/main/java/com/acciojob/librarymanagementsystemApril/LibraryManagementSystemAprilApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LibraryManagementSystemAprilApplication {
+public class LibraryManagementSystemAprilApplication { //this is my main funtion
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryManagementSystemAprilApplication.class, args);
